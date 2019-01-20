32c02f8d6f5b9983c3e4897e616117f075248fdd
ElektrikSpark <elektrikspark@gmail.com>
2019-01-20 07:28:15 -0600

    added fully functional lambda script with sns email

--- serverless-portfolio-with-react.py
+++ serverless-portfolio-with-react.py
@@ -3,18 +3,27 @@ import boto3
 import zipfile
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
-
-    build_bucket = s3.Bucket('build.portfolio.spark.info')
-    portfolio_bucket = s3.Bucket('portfolio.spark.info')
-
-    build_bucket.download_file('buildPortfolio', '/tmp/portfolio.zip')
-
-    with zipfile.ZipFile('/tmp/portfolio.zip') as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm)
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:145718245307:deployPortfolio')
+    
+    try:
+        s3 = boto3.resource('s3')
+    
+        build_bucket = s3.Bucket('build.portfolio.spark.info')
+        portfolio_bucket = s3.Bucket('portfolio.spark.info')
+    
+        build_bucket.download_file('buildPortfolio', '/tmp/portfolio.zip')
+    
+        with zipfile.ZipFile('/tmp/portfolio.zip') as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
+    except: 
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed successfully...")
+        raise
     
     return {
         'statusCode': 200,
