095066b4b10dee0ef73763b4faa8103b0749af7b
ElektrikSpark <elektrikspark@gmail.com>
2019-01-20 06:22:23 -0600

    added fully functional lambda script

--- serverless-portfolio-with-react.py
+++ serverless-portfolio-with-react.py
@@ -1,15 +1,22 @@
+import json
 import boto3
 import zipfile
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-build_bucket = s3.Bucket('build.portfolio.spark.info')
-portfolio_bucket = s3.Bucket('portfolio.spark.info')
+    build_bucket = s3.Bucket('build.portfolio.spark.info')
+    portfolio_bucket = s3.Bucket('portfolio.spark.info')
 
-build_bucket.download_file('buildPortfolio.zip', '/tmp/portfolio.zip')
+    build_bucket.download_file('buildPortfolio', '/tmp/portfolio.zip')
 
-with zipfile.ZipFile('/tmp/portfolio.zip') as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm)
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile('/tmp/portfolio.zip') as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
